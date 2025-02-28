818d53c0bf65ef1930c0e6ae4ed673707f3cdee8
João Pedro Scarmignani Lumi <jpedro.lumi@gmail.com>
2025-02-28 12:12:35 -0300

    initial setup

---
+++ location-service/src/main/java/com/jplumi/controller/LocationController.java
@@ -0,0 +1,15 @@
+package com.jplumi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+
+    @GetMapping("/location")
+    public String sendCurrentLocation() {
+        return "hello";
+    }
+
+}
