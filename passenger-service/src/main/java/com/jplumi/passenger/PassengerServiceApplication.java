b9936655255652e574457aaa64590abd4e3d87d0
João Pedro Scarmignani Lumi <jpedro.lumi@gmail.com>
2025-03-21 10:52:10 -0300

    Add eureka server and feign client

--- passenger-service/src/main/java/com/jplumi/passenger/PassengerServiceApplication.java
+++ passenger-service/src/main/java/com/jplumi/passenger/PassengerServiceApplication.java
@@ -2,8 +2,10 @@ package com.jplumi.passenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PassengerServiceApplication extends SpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(PassengerServiceApplication.class, args);
--- passenger-service/src/main/java/com/jplumi/passenger/client/TripManagementClient.java
+++ passenger-service/src/main/java/com/jplumi/passenger/client/TripManagementClient.java
@@ -1,5 +1,11 @@
 package com.jplumi.passenger.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("travel-management-service")
 public interface TripManagementClient {
-    boolean validateTripSubscription(Long tripId, Integer stopNumber);
+    @GetMapping("/trips/validate-subscription")
+    boolean validateTripSubscription(@RequestParam Long tripId, @RequestParam Integer stopNumber);
 }
